293387ce7c6a07b81f4ca0a7ca2cce6ed340feab
Travis Chase <gaudeon@cpan.org>
2015-07-09 23:13:20 -0600

    motor test

--- app.js
+++ app.js
@@ -3,4 +3,31 @@
 var ev3dev = require("ev3dev"),
     _      = require("lodash");
 
+var motor = new ev3dev.Motor();
 
+if (!motor.connected)
+    console.log("No motor could be found. Are you sure that one is connected?");
+
+console.log(' Port: ' + motor.portName);
+console.log(' Driver: ' + motor.driverName);
+
+console.log('Setting motor properties...');
+motor.rampUpSp = 100;
+motor.rampDownSp = 100;
+motor.timeSp = 1000;
+motor.dutyCycleSp = 50;
+console.log('Available commands: ' + motor.commands);
+console.log('Sending motor command...');
+motor.command = 'run-timed';
+
+do {
+    console.log("Motor speed: " + motor.speed);
+    
+    {   //Hack to sleep for time
+        //    SHOULD NOT BE USED IN PRODUCTION CODE
+        var start = new Date().getTime();
+        while (new Date().getTime() < start + 80) {
+            ;
+        }
+    }
+} while(motor.speed > 10);
